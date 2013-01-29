50674795e64e0f0b8dc38b1bf11ebc022850973a
alop <alopgeek@gmail.com>
2013-01-28 23:14:53 -0800

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "al592b@att.com"
 license          "All rights reserved"
 description      "pacct forwarding to loghost"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 recipe            "pacct-log::default", "Installs rsync"
 recipe            "pacct-log::server", "Configures rsync, sets up logdirs"
 recipe            "pacct-log::client", "Configures rsync, sets up forwarding"
